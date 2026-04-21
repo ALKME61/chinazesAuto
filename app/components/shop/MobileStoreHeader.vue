4c0028738e88365dfc29f231a44f753be2bd71d4
ALKME61 <karim.boev3@gmail.com>
2026-04-21 15:26:08 +0300

    f

--- app/components/shop/MobileStoreHeader.vue
+++ app/components/shop/MobileStoreHeader.vue
@@ -43,7 +43,7 @@ const searchValue = ref('')
 <style scoped lang="scss">
 .mobile-store-header {
   background-color: white;
-  padding-top: 300px;
+  padding-top: env(safe-area-inset-top);
 }
 
 .mobile-store-header__shell {
